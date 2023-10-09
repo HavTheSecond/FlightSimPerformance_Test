bafc38c26a6143a1490312ad07efe49e88863143
Paul Schuetz <pa.schuetz@web.de>
2023-10-09 20:29:43 +0200

    Initial Commit

---
+++ FlightSimPerformance/FlightSimPerformanceApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlightSimPerformanceApp.swift
+//  FlightSimPerformance
+//
+//  Created by Paul on 2023-10-09.
+//
+
+import SwiftUI
+
+@main
+struct FlightSimPerformanceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
